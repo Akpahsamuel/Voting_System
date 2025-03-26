3837e508809014a80d9bf2a171e232272f072992
Akpahsamuel <sakpah@st.ug.edu.gh>
2025-03-26 23:59:00 +0000

    adding wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { useTheme } from "./providers/theme/ThemeContext";
 import ProposalView from "./views/ProposalView";
+import Navbar from "./components/Navbar";
+import WalletView from "./views/WalletView";
+import { useNavigation } from "./providers/navigation/NavigationContext";
+
+const Pages = () => {
+  const { currentPage } = useNavigation();
+
+  switch(currentPage) {
+    case "/":
+      return <ProposalView />
+    case "/wallet":
+      return <WalletView />
+    default:
+      return <div className="text-center">Page not found!</div>
+  }
+};
 
 const App: React.FC = () => {
   const { darkMode }  = useTheme();
 
   return (
     <div className={`${darkMode ? "dark" : ""}`}>
-      <div className="min-h-screen  bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-        <div className="max-w-screen-xl mx-auto pt-16">
-          <h1 className="text-4xl font-bold ">Simple Proposal App </h1>
-          <ProposalView />
+      <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+        <Navbar />
+        <div className="max-w-screen-xl m-auto pt-16">
+          <Pages />
         </div>
-       
       </div>
     </div>
   );
--- frontend/src/views/ProposalView.tsx
+++ frontend/src/views/ProposalView.tsx
@@ -1,10 +1,10 @@
 
 
-const PROPOSAL_COUNT = 8;
+const PROPOSAL_COUNT = 7;
 
 const ProposalItem = () => {
   return (
-    <div className="p-4 border rounded-lg shadow-sm big-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
+    <div className="p-4 border rounded-lg shadow-sm bg-white dark:bg-gray-800 hover:border-blue-500 transition-colors">
       <p className="text-xl font-semibold mb-2">Title: Hello There</p>
       <p className="text-gray-700 dark:text-gray-300">Desc: What is your vote ?</p>
     </div>
@@ -14,11 +14,14 @@ const ProposalItem = () => {
 const ProposalView = () => {
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      { new Array(PROPOSAL_COUNT).fill(1).map((id) =>
-        <ProposalItem key={id * Math.random()}/>
-      )}
-    </div>
+    <>
+      <h1 className="text-4xl font-bold mb-8">New Proposals</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        { new Array(PROPOSAL_COUNT).fill(1).map((id) =>
+          <ProposalItem key={id * Math.random()}/>
+        )}
+      </div>
+    </>
   )
 }
 
